823b722a0e6cf79f4832e2f3e026702aced4c39b
Luca Gesmundo <lucagesmundo@yahoo.it>
2018-10-18 10:20:20 +0200

    moved to transition in place of animation

--- bars.js
+++ bars.js
@@ -4,11 +4,11 @@ const progress = document.querySelectorAll('.progress');
 // console.log(bars[1].style.width);
 
 bars.forEach((bar, index) => {
-  const randomWidth = Math.floor((Math.random() * 65) + 20);
+  const randomWidth = Math.floor((Math.random() * 60) + 20);
   bar.style.width = `${randomWidth}%`;
 
   progress[index].addEventListener('mouseover', () => {
-    const randomTiming = Math.floor((Math.random() * 3) + 2);
+    const randomTiming = Math.floor((Math.random() * 2) + 2);
     console.log(randomTiming);
     bar.style.transitionDuration = `${randomTiming}s`;
     bar.style.width = '100%';
